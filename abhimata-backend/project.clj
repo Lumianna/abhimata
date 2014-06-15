7e410b6b40318ecfc8feaa2c45272a3077982099
Kristian Nybo <kristian.nybo@futurice.com>
2014-06-15 22:37:50 +0300

    Initial commit for clojure backend

---
+++ abhimata-backend/project.clj
@@ -0,0 +1,7 @@
+(defproject abhimata-backend "0.1.0-SNAPSHOT"
+  :description "Backend for abhimata"
+  :url "https://github/kryft/abhimata"
+  :license {:name "GPL3" }
+  :dependencies [[org.clojure/clojure "1.6.0"
+                  ring/ring-core "1.3.0"
+                  ring/ring-jetty-adapter "1.3.0"]])
