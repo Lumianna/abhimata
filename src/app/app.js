5eb677b65ee584755552874d44340a2242650059
Kristian Nybo <kristian.nybo@futurice.com>
2014-07-07 00:29:20 +0300

    Remove ngbp stuff, fix tests

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,14 @@
-angular.module( 'ngBoilerplate', [
+angular.module( 'abhimata', [
+  'ngRoute',
   'templates-app',
-  'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.router'
+  'templates-common'
 ])
-
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
-})
 
 .run( function run () {
 })
 
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.message = "hullo";
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,17 +1,16 @@
 describe( 'AppCtrl', function() {
-  describe( 'isCurrentUrl', function() {
     var AppCtrl, $location, $scope;
 
-    beforeEach( module( 'ngBoilerplate' ) );
+    beforeEach( module( 'abhimata' ) );
 
     beforeEach( inject( function( $controller, _$location_, $rootScope ) {
       $location = _$location_;
       $scope = $rootScope.$new();
       AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
-    }));
+    })); 
 
-    it( 'should pass a dummy test', inject( function() {
-      expect( AppCtrl ).toBeTruthy();
+    it( 'message should be hullo', inject( function() {
+      expect( $scope.message ).toEqual('hullo');
     }));
-  });
+    
 });
