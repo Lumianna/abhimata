a1e55a93246f78e6a36addf688cec6f446c0fab6
Kristian Nybo <kristian.nybo@futurice.com>
2014-07-06 20:51:42 +0300

    add friend

--- abhimata-backend/project.clj
+++ abhimata-backend/project.clj
@@ -2,6 +2,9 @@
   :description "Backend for abhimata"
   :url "https://github/kryft/abhimata"
   :license {:name "GPL3" }
+  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [ring/ring-core "1.3.0"]
-                  [ring/ring-jetty-adapter "1.3.0"]])
+                 [com.cemerick/friend "0.2.1"]
+                 [ring/ring-core "1.3.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.3.0"]])
--- abhimata-backend/src/abhimata_backend/core.clj
+++ abhimata-backend/src/abhimata_backend/core.clj
@@ -1,6 +1,16 @@
-(ns abhimata-backend.core)
+(ns abhimata-backend.core
+  (:require [cemerick.friend :as friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds])
+            [compojure.core :as compojure :refer (GET POST ANY defroutes)]
+            (compojure [handler :as handler]
+                       [route :as route])))
 
-(defn hanndler [request]
+(def users (atom {"admin" { :username "admin"
+                           :password (creds/hash-bcrypt "clojure")
+                           :roles #{::user}}}))
+
+(defn handler [request]
   {:status 200
    :header {"Content-Type" "text/html"}
    :body "Hello, World!" })
