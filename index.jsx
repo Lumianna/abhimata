ae51a033c683c5aa388fad2a9422de49abcdebdf
Kristian Nybo <kristian.nybo@futurice.com>
2014-07-30 21:32:26 +0300

    First draft of EditableFormComponent

--- index.jsx
+++ index.jsx
@@ -2,30 +2,77 @@
 var React = require('react');
 var pkg = require('./package.json');
 
+
+var FormElementSelector = React.createClass({
+
+  formElements : [
+    { type : "text", label : "Single-line textbox"},
+    { type : "textarea", label : "Multi-line textbox"},
+    //{ type : "radio", label : "Radio button" },
+  ], 
+
+  getInitialState : function() {
+    return {type : "textarea"};
+  },
+
+  handleRadioClick : function(event) {
+    this.setState({type : event.target.value});
+    console.log(event.target.value);
+  },
+
+  addFormElement : function() {
+    this.props.onSelection(this.state.type);
+  },
+
+  render : function() {
+    var radioButtons = this.formElements.map(function(elem) {
+      console.log(this);
+      return ( <label> 
+        <input type="radio" name="elementSelector" 
+          value={elem.type} onClick={this.handleRadioClick} />
+        {elem.label}
+        </label>
+             );
+    }.bind(this));
+    return (
+      <div className="formElementSelector">
+        {radioButtons}
+        <button onClick={this.addFormElement}> Add form element </button>
+      </div>
+    );
+  }
+});
+
 var EditableForm = React.createClass({
   getInitialState: function() {
     return {
       elements : []
     };
   },
-  
-  addTextBox : function() {
-    var newElements = this.state.elements.concat(["textbox"]);
+
+  addFormElement : function(elem) {
+    var newElements = this.state.elements;
+    newElements.push(elem);
     this.setState({elements : newElements});
   },
-    
+
   render: function() {
     var components = this.state.elements.map(function(elem) {
-      if (elem === "textbox") {
-        return ( <input type="text" /> );
+      switch(elem) {
+        case  "textarea" :
+          return ( <textarea rows="4" /> );
+        break;
+        case  "text" :
+          return ( <input type="text" /> );
+        break;
+        default :
+          return null;
       }
     });
 
     return (
       <div className="editableForm"> 
-        <button onClick={this.addTextBox}>
-          Moar boxen!
-        </button>
+        <FormElementSelector onSelection={this.addFormElement}/>
         {components}
       </div>
     );
