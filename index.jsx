1b170e953634e4bbb361d3b36db2c511e18b6ae7
Kristian Nybo <kristian.nybo@futurice.com>
2014-09-14 21:03:59 +0300

    add react-router

--- index.jsx
+++ index.jsx
@@ -4,7 +4,45 @@ var pkg = require('./package.json');
 
 var EditableForm = require('./editableform.jsx');
 
-React.renderComponent(
-  <EditableForm />
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var Routes = Router.Routes;
+var Link = Router.Link;
 
-, document.body);
+
+var RetreatList = React.createClass({
+  render : function() {
+    return ( <p>A list of retreats!</p> );
+  }
+});
+
+var App = React.createClass({
+  render : function() {
+    return (
+      <div>
+        <header>
+          <ul>
+            <li> <Link to="retreats">List of retreats </Link> </li>
+            <li> <Link to="formeditor">Form editor </Link> </li>
+          </ul>
+        </header>
+
+        <this.props.activeRouteHandler/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Routes location="hash">
+    <Route name="app" path="/" handler={App}>
+      <Route name="retreats" handler={RetreatList}/>
+      <Route name="formeditor" handler={EditableForm}/>
+      <DefaultRoute handler={RetreatList}/>
+      </Route>
+  </Routes>
+);
+
+
+React.renderComponent(routes, document.body);
